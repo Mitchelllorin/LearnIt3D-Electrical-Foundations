eec8a903ec9e59c7f224a5a36d894572914ef34e
Mitchell Lorin Mcknight <mitchelllorinmcknight@gmail.com>
2026-06-01 22:39:39 -0700

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { route, currentPage } from './routes.js';
+    import ModuleList from './pages/ModuleList.svelte';
+    import Lesson from './pages/Lesson.svelte';
+</script>
+
+{#if $currentPage.name === 'modules'}
+    <ModuleList />
+{:else if $currentPage.name === 'lesson'}
+    <Lesson moduleId={$currentPage.moduleId} lessonId={$currentPage.lessonId} />
+{/if}
