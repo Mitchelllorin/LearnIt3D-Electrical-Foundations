db2259d20df2acbb8e22f635395b14358d1bc97d
Mitchell Lorin Mcknight <mitchelllorinmcknight@gmail.com>
2026-06-01 22:42:03 -0700

    Create Lesson.svelte

---
+++ src/pages/Lesson.svelte
@@ -0,0 +1,9 @@
+<script>
+    import MarkdownViewer from '../components/MarkdownViewer.svelte';
+    export let moduleId;
+    export let lessonId;
+
+    let filePath = `/src/content/${moduleId}.md`;
+</script>
+
+<MarkdownViewer file={filePath} />
