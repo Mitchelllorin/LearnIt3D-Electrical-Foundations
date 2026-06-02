121452a98cc8bf3660eb2fa68eb16d3ac18e60ac
Mitchell Lorin Mcknight <mitchelllorinmcknight@gmail.com>
2026-06-01 22:47:18 -0700

    Add MarkdownViewer component to render Markdown files

---
+++ src/components/MarkdownViewer.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { onMount } from 'svelte';
+    import { marked } from 'marked';
+
+    export let file;
+    let html = '';
+
+    onMount(async () => {
+        const res = await fetch(file);
+        const text = await res.text();
+        html = marked(text);
+    });
+</script>
+
+<div class="lesson" bind:this={container}>
+
+    {@html html}
+
+</div>
+
+<style>
+.lesson {
+    padding: 20px;
+    font-size: 1.1rem;
+    line-height: 1.6;
+}
+</style>
+I don't understand why you don't know I guess the most basic of things you just sat here and fucking made the fucking thing with me and you don't even know that it's module 01:02 03
